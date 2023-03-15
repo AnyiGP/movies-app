06e371e708b844fc0d749c4a7bfc12b2149c7d26
AnyiGP <angelesgp31@hotmail.com>
2023-03-15 12:17:21 -0300

    Empiezo axios

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,33 @@
 import { Layout } from "../../components";
+import { useEffect, useState } from "react";
+import { apiMovies } from "../../utils/axios";
+import { MovieType } from '../../types'
 
 const Home = () => {
+  //   //ejemplo de movies, creo un estado
+  const [movies, setMovies] = useState([]);
+
+  //   //pido a la api
+  useEffect(() => {
+    apiMovies.get("/movie/top_rated").then((response) =>
+      setMovies(response.data.results));
+    //   console.log(response.data.results)
+    // );
+  }, []);
+
   return (
     <>
-      <Layout>Home{/* Slide 5 pelis+ Carrousel populares 10 pelis + Carrousel ultimos-lanzamientos 10 pelis */}</Layout>
+      <Layout>
+        Home
+        <ul>
+          {/* Slide 5 pelis+ Carrousel populares 10 pelis + Carrousel ultimos-lanzamientos 10 pelis */}
+          {movies.map((movie) => (
+            <li>
+                {/* {movie.title} */}
+                </li>
+          ))}
+        </ul>
+      </Layout>
     </>
   );
 };
---
+++ src/services/moviesService.ts
@@ -0,0 +1,13 @@
+import {} from "../helpers/mapToArray";
+import { MovieType } from "../types";
+import { api, apiMovies } from "../utils/axios";
+
+//trae todas las pelis
+const getAll = async () => {
+  const response = await apiMovies.get("/users.json");
+
+  //   console.log(response)
+  return response.data;
+};
+
+export const serviceUsers = { getAll };
