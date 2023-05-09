f8d07e3b2eec3bbf97300c4b341fc520621d3f61
AnyiGP <angelesgp31@hotmail.com>
2023-05-09 16:28:52 -0300

    agrego onClick y useNavigate a card-movie y card-scroll para agarrar la moviey mostrar detalles. Termino mostrar detalles. Falta estilado

--- src/components/commons/card-movie/index.tsx
+++ src/components/commons/card-movie/index.tsx
@@ -1,24 +1,30 @@
 import { FC } from "react";
 import { Button, Card, Col, Row } from "react-bootstrap";
 import { Movie, PartialMovies } from "../../../types";
+import { Navigate, useNavigate } from "react-router-dom";
 
 const CardMovie: FC<PartialMovies> = ({ items }) => {
+  const navigate = useNavigate();
+
   return (
     <Row xs={1} md={2} className="g-4">
       {items?.map((item: any) => (
         <Col>
           <Card>
             <Card.Img
+              key={item.id}
+              onClick={() => navigate(`/movie/${item.id}`)}
+              className="d-block w-100"
               variant="top"
-              src={`${`https://image.tmdb.org/t/p/original` + item.backdrop_path}`}
+              src={`${
+                `https://image.tmdb.org/t/p/original` + item.backdrop_path
+              }`}
               alt=""
             />
             <Card.Body>
               <Card.Title>{item.title}</Card.Title>
               // <Card.Text>{item.id}</Card.Text>
-              <Card.Text>
-                Descripción
-              </Card.Text>
+              <Card.Text>Descripción</Card.Text>
             </Card.Body>
           </Card>
         </Col>
